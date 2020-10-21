3e5d9f1bf1e5d64b40cead19ace7722b864bca35
Mark Tripney <mrktrpny@gmail.com>
2020-10-21 20:28:27 +0200

    Add option to close modal on 'click outside' or via Escape key

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const cardButtons = document.querySelectorAll('.card button');
-const modalOuter = document.querySelector('.modal-outer');
-const modalInner = document.querySelector('.modal-inner');
+const modalBackground = document.querySelector('.modal-background');
+const modalForeground = document.querySelector('.modal-foreground');
 
 function handleCardButtonClick(e) {
   const button = e.currentTarget;
@@ -8,14 +8,36 @@ function handleCardButtonClick(e) {
   const imgSrc = card.querySelector('img').src;
   const desc = card.querySelector('img').alt;
   // Populate the modal
-  modalInner.innerHTML = `
+  modalForeground.innerHTML = `
     <img src="${imgSrc.replace('200', '600')}" alt="${desc}" />
     <p>${desc}</p>
     `;
   // Show the modal
-  modalOuter.classList.add('open');
+  modalBackground.classList.add('open');
+}
+
+function closeModal() {
+  modalBackground.classList.remove('open');
 }
 
 cardButtons.forEach((button) => {
   button.addEventListener('click', handleCardButtonClick);
 });
+
+modalBackground.addEventListener('click', function (e) {
+  // If user clicks outside modal (no closest 'modal-foreground' class), return true
+  const outOfModal = !e.target.closest('.modal-foreground');
+  if (outOfModal) {
+    closeModal();
+  }
+});
+
+window.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && modalBackground.classList.contains('open')) {
+    closeModal();
+  }
+});
+
+if (modalBackground.classList.contains('open')) {
+  console.log('OPEN');
+}
